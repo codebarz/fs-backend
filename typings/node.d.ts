e0fc0a3772e9f5b751d49b3af66c157ffa10244a
whitehox <oketegah@gmail.com>
2020-07-26 23:54:06 +0100

    ⬆️ update node env type def

--- typings/node.d.ts
+++ typings/node.d.ts
@@ -2,5 +2,11 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     NODE_ENV: 'development' | 'production' | 'test';
     DATABASE_URL: string;
+    ACCESS_TOKEN_SECRET: string;
+    BCRYPT_SALT_ROUNDS: string;
+    SENDGRID_API_KEY: string;
+    CLIENT_URL: string;
+    APP_MAIL_ADDRESS: string;
+    VERIFICATION_MAIL_TEMPLATE_ID: string;
   }
 }
