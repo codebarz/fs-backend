7317a8eff5039de18a08ba276eb8d262bfa395f8
whitehox <oketegah@gmail.com>
2020-07-27 08:40:14 +0100

    ⚙️ pass signup resolver to mutation

--- src/schema.ts
+++ src/schema.ts
@@ -1,7 +1,9 @@
 import { GraphQLObjectType, GraphQLSchema } from 'graphql';
-
 import sampleController from './controllers/sample';
 import SampleType from './types/sample';
+import { UserInputType } from './types/user.ts/type';
+import { SignupType } from './types/auth/type';
+import { auth } from './controllers';
 
 const query = new GraphQLObjectType({
   name: 'Query',
@@ -15,8 +17,26 @@ const query = new GraphQLObjectType({
   }),
 });
 
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  description: 'The query root for fs',
+  fields: () => ({
+    signup: {
+      type: SignupType,
+      description: 'The mutation for signup',
+      args: { data: { type: UserInputType } },
+      resolve: async (_, { data }): Promise<{ token: string }> => {
+        const response = await auth.signup(data);
+
+        return { token: response };
+      },
+    },
+  }),
+});
+
 const schema = new GraphQLSchema({
   query,
+  mutation,
 });
 
 export default schema;
