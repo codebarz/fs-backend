22aede8754d808ca8de8783c2224cd7839652557
whitehox <oketegah@gmail.com>
2020-07-27 08:37:23 +0100

    ⚙️implement barelling for routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,1 @@
-import { Router } from 'express';
-import sampleController from '../controllers/sample';
-
-const router = Router();
-
-router.get('/', function(_req, res, _next) {
-  const message = sampleController();
-
-  res.status(200).json({ message });
-});
-
-export default router;
+export * as authRouter from './auth'
